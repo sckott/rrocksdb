ca3f486902107c4eef7a28f31c0034906cda67f3
Scott Chamberlain <myrmecocystus@gmail.com>
2018-05-31 10:54:09 -0700

    test file

--- src/test.cpp
+++ src/test.cpp
@@ -11,15 +11,15 @@ using namespace rocksdb;
 std::string foo() {
   Options options;
   options.create_if_missing = true;
-  // DB* db;
-  // rocksdb::Status status =
-  //   rocksdb::DB::Open(options, "/tmp/testdb", &db);
-  // assert(status.ok());
-  // std::string out;
-  // if (status.ok()) {
-  //   out = status.ToString();
-  // };
-  // return out;
-  std::string out = "xx";
+  DB* db;
+  rocksdb::Status status =
+    rocksdb::DB::Open(options, "/tmp/testdb", &db);
+  assert(status.ok());
+  std::string out;
+  if (status.ok()) {
+    out = status.ToString();
+  };
   return out;
+  // std::string out = "xx";
+  // return out;
 };
